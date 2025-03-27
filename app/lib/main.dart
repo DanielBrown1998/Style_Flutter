4ac6de0790fbdae630b4262b0da3a8ad1cb0a45b
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-03-26 21:16:35 -0300

    Enhance app theme with Material3 and custom text styles

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -10,9 +10,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    MaterialColor primaryColor = Colors.purple;
+
     return MaterialApp(
       title: 'AluraBank',
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: primaryColor,
+        primaryColor: primaryColor,
+        brightness: Brightness.dark,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(
+            fontFamily: "Roboto",
+            fontSize: 20,
+            fontWeight:  FontWeight.bold
+          ),
+          headlineSmall: TextStyle(
+            fontFamily: "Roboto",
+          ),
+          headlineMedium: TextStyle(
+            fontFamily: "Roboto",
+            fontSize: 30,
+            fontWeight: FontWeight.w500
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 16,
+          )
+        ), 
+      ),
       home: const Home(title: 'AluraBank'),
     );
   }
